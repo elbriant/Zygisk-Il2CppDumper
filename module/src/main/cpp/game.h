0581167b0d96a59e40b6b100590ffbc7900c1dff
elbriant <elbriant14@gmail.com>
2026-02-23 12:18:14 -0400

    fix github actions builder, and set default package name to yostarjp (arknights jp)

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.YoStarEN.Arknights"
+#define GamePackageName "com.YoStarJP.Arknights" // using jp bc updates come a little bit sooner
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
